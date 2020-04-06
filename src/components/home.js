0e9fdf4d4bde0ac0ad45945a7a1c8c548621abbf
Hardik Ahalpara <hardik.j.ahalpara@gmail.com>
2020-04-06 22:12:10 +0530

    prefinal

--- src/components/home.js
+++ src/components/home.js
@@ -8,6 +8,7 @@ class home extends Component {
     return (
 
       <div className="homeContainer">
+        <p style={{ top: '10%', left: '8%', lineHeight: '25px' }} class="htmltags" id="info">&lt;!--mobile version is still under development as few components are yet to be made responsive, continue on Desktop for better experience.--&gt;</p>
         <p style={{ top: '5%', left: '5%' }} class="htmltags">&lt;html&gt;</p>
         <p style={{ top: '13%', left: '7%' }} class="htmltags">&lt;body&gt;</p>
         <p style={{ top: '30%', left: '10%' }} class="htmltags">&lt;h1&gt;</p>
